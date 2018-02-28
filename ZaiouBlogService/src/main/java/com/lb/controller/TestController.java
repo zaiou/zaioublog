f50f5921bdadc206fdf3a3fabb9affb823a0f09a
LB <1032809692@qq.com>
2018-02-28 13:54:09 +0800

    添加logback

--- ZaiouBlogService/src/main/java/com/lb/controller/TestController.java
+++ ZaiouBlogService/src/main/java/com/lb/controller/TestController.java
@@ -2,6 +2,8 @@ package com.lb.controller;
 
 import com.lb.model.Admin;
 import com.lb.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/test")
 public class TestController {
+    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
     @Autowired
     private  TestService testService;
 
     @RequestMapping(value = "/find")
     @ResponseBody
     public Admin find(){
+        logger.info("添加日志测试");
         return  testService.find();
     }
 }
